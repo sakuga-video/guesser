ed090da4201f38f4a8548c27ad498f84e6eafd7d
Cypress Frankenfeld <iamcypress@gmail.com>
2021-03-07 13:32:37 -0500

    Use Material UI components for input

--- src/Guess.tsx
+++ src/Guess.tsx
@@ -1,3 +1,4 @@
+import { TextField } from '@material-ui/core';
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 type Props = {
@@ -11,10 +12,12 @@ const Guess = ({ on_guess_submitted }: Props) => {
     event.preventDefault();
     on_guess_submitted(guess);
   }
+
+  const on_guess_change = (event: ChangeEvent<HTMLInputElement>) => set_guess(event.target.value);
   
   return (
     <form id="guess" className="controls" onSubmit={submit_guess}>
-      <input type="text" placeholder="Guess the title" onChange={(event: ChangeEvent<HTMLInputElement>) => set_guess(event.target.value)} />
+      <TextField label="Guess the title" onChange={on_guess_change} />
     </form>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const dark_theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={dark_theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
