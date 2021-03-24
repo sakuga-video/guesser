829b3c04785aee41c60410bcc972f9ec16cc993c
Cypress Frankenfeld <iamcypress@gmail.com>
2021-03-23 21:30:31 -0400

    Add story for GameSummary, use material UI

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,19 @@
+import { ThemeProvider } from '@material-ui/styles';
+import '../src/App.css';
+import { Provider } from 'react-redux';
+import { dark_theme } from '../src/index';
+import { store } from '../src/app/store';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={dark_theme}>
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    </ThemeProvider>
+  )
+]
--- src/GameSummary.tsx
+++ src/GameSummary.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@material-ui/core";
+import { Button, Card, CardActionArea, CardContent, CardMedia, Container, Grid, Paper, Typography } from "@material-ui/core";
+import React from "react";
 import { choose_random_tags, Tag, useThunkDispatch } from "./App";
 import { start } from "./appSlice";
 import Matches, { Guess } from "./GuessMatcher";
 import { Video } from "./VideoWrapper";
 
-type Props = {
+export type GameSummaryProps = {
     rounds: Round[],
     all_tags: Tag[],
 };
@@ -23,40 +24,53 @@ const render_guess = (guess: Guess) => {
     if (!guess.guess) {
         return "No guess";
     }
-    return "Guess " + guess.guess + " " + (Matches(guess).matches ? "🎉 was correct 🎊" : "was incorrect");
+    return "Guess \"" + guess.guess + "\" " + (Matches(guess).matches ? "🎉 was correct 🎊" : "was incorrect");
 }
 
 const video_thumbnails = (videos: Video[]) =>
     videos.map(video =>
         <li key={video.id}>
             <a href={SAKUGABOORU_VIDEO_URL + video.id}>
-                <img className="thumbnail" alt={"Thumbnail preview of " + video.tags[0]} src={video.preview_url} />
+                <img className="thumbnail" alt={"Thumbnail preview of " + video.tags[0].name} src={video.preview_url} />
             </a>
         </li>
     )
 
 const round_summary = (round: Round, index: number) => {
     return (
-        <li key={index}>
-            <h1><a href={SAKUGABOORU_TAG_URL + round.tag.name.replaceAll(" ", "_")}>{round.tag.name}</a></h1>
-            <p>{render_guess(round.guess)}</p>
-            <ol>
-                {video_thumbnails(round.videos)}
-            </ol>
-        </li>
+        <Grid key={index} item className="round-summary">
+            <Card>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        title={"Image thumbnail of a clip from " + round.videos[0].tags[0].name}
+                        alt={"Image thumbnail of a clip from " + round.videos[0].tags[0].name}
+                        image={round.videos[0].preview_url}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            <a href={SAKUGABOORU_TAG_URL + round.tag.name.replaceAll(" ", "_")}>{round.tag.name}</a>
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            {render_guess(round.guess)}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
     );
 }
 
-const GameSummary = ({rounds, all_tags}: Props) => {
+const GameSummary = ({rounds, all_tags}: GameSummaryProps) => {
     const dispatch = useThunkDispatch();
 
     return (
-        <div id="round-summary">
-            <ol>
+        <Container id="game-summary" maxWidth="lg">
+            <Grid container spacing={2}>
                 {rounds.map(round_summary)}
-            </ol>
-            <Button variant="contained" onClick={() => dispatch(start(choose_random_tags(all_tags)))}>Restart</Button>
-        </div>
+            </Grid>
+            <Button variant="contained" onClick={() => dispatch(start(choose_random_tags(all_tags)))}>Play Again</Button>
+        </Container>
     )
 }
 
