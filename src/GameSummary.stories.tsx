477d6106111ed33e8339f1d210545e2db799b26b
Cypress Frankenfeld <iamcypress@gmail.com>
2021-03-24 15:53:38 -0400

    Remove unused example storybooks
    Also move the GameSummary.stories.tsx to the root level src file. I will keep it in the same folder as GameSummary.tsx, if I later put that in its own folder, for ease of finding it.

--- src/GameSummary.stories.tsx
+++ src/GameSummary.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import GameSummary, { GameSummaryProps } from '../GameSummary';
-import { TagType } from '../App';
+import GameSummary, { GameSummaryProps } from './GameSummary';
+import { TagType } from './App';
 
 
 export default {
