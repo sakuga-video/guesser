3ae3ce64d807661e0f268693c308778975a01708
Cypress Frankenfeld <iamcypress@gmail.com>
2021-03-31 11:17:34 -0400

    Paginate game history

--- src/History.tsx
+++ src/History.tsx
@@ -1,35 +1,53 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useThunkDispatch } from "./App";
 import { RootState } from "./app/store";
 import { DatabaseGuess } from "./GuessDatabase";
-import { load_guesses } from "./historySlice";
+import { load_guesses, load_num_pages } from "./historySlice";
 import { sortBy } from 'lodash';
+import { Pagination } from "@material-ui/lab";
+import { Container, Grid } from "@material-ui/core";
+
+const PAGE_SIZE = 12;
 
 const History = () => {
     const dispatch = useThunkDispatch();
-    const { guesses } = useSelector((state: RootState) => state.history);
-    
-    useEffect(() => dispatch(load_guesses()), []);
+    const { guesses, num_guesses, page } = useSelector((state: RootState) => state.history);
+
+    useEffect(() => dispatch(load_num_pages()), [dispatch]);
+    useEffect(() => dispatch(load_guesses({ page: 0, page_size: PAGE_SIZE })), [dispatch]);
+
+    const on_page_change = (_: React.ChangeEvent<unknown>, page: number) => {
+        dispatch(load_guesses({page: page - 1, page_size: PAGE_SIZE}));
+    }
 
     return (
-        <ol>
-            {guesses.map(guess_ui)}
-        </ol>
-    );
+        <Container>
+            <Grid container spacing={2}>
+                {guesses.map(guess_ui)}
+            </Grid>
+            <Pagination
+                page={page + 1}
+                count={Math.ceil(num_guesses / PAGE_SIZE)}
+                onChange={on_page_change}
+            />
+        </Container>
+
+    )
 };
 
 const guess_ui = (guess: DatabaseGuess) => {
     const last_video = guess.videos[guess.videos.length - 1];
     const tags = sortBy(last_video.tags, ["count"]);
     const title = tags[tags.length - 1].name;
+
     return (
-        <li>
+        <Grid key={guess.id} item className="round-summary" xs={12} sm={6} md={4}>
             <p>Title: <a href={"https://www.sakugabooru.com/post?tags=" + title.split(" ").join("_")}>{title}</a></p>
             <p>Guess: {guess.guess}</p>
             <p>Date: {new Date(guess.date).toDateString()}</p>
             <a href={last_video.url}><img src={last_video.preview_url} /></a>
-        </li>
+        </Grid>
     );
 }
 
--- src/historySlice.ts
+++ src/historySlice.ts
@@ -5,29 +5,52 @@ import { DatabaseGuess } from "./GuessDatabase";
 
 interface HistoryState {
     readonly guesses: DatabaseGuess[],
+    readonly page: number,
+    readonly num_guesses: number,
 }
 
 const initialState: HistoryState = {
     guesses: [],
+    page: 0,
+    num_guesses: 0,
 }
 
 export const historySlice = createSlice({
     name: 'history',
     initialState,
     reducers: {
-        show_guesses: (state, action: PayloadAction<DatabaseGuess[]>) => {
-            state.guesses = action.payload;
-        }
+        set_guesses: (state, action: PayloadAction<{page: number, guesses: DatabaseGuess[]}>) => {
+            state.guesses = action.payload.guesses;
+            state.page = action.payload.page;
+        },
+        set_num_guesses: (state, action: PayloadAction<number>) => {
+            state.num_guesses = action.payload;
+        },
     }
 });
 
 export const {
-    show_guesses,
+    set_guesses,
+    set_num_guesses,
 } = historySlice.actions;
 
-export const load_guesses = (): AppThunk => dispatch => {
-    database.guesses.toArray()
-        .then(guesses => dispatch(show_guesses(guesses)));
+export const load_num_pages = (): AppThunk => dispatch => {
+    database.guesses.count()
+        .then(count => dispatch(set_num_guesses(count)));
 }
+
+type Page = { page: number, page_size: number };
+
+export const load_guesses =
+    ({ page, page_size }: Page): AppThunk =>
+    dispatch => {
+        database.guesses
+            .orderBy("date")
+            .reverse()
+            .offset(page * page_size)
+            .limit(page_size)
+            .toArray()
+            .then(guesses => dispatch(set_guesses({page, guesses})));
+    }
 
 export default historySlice.reducer;
